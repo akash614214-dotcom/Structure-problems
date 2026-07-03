3657bb09a4f99d082d89cf7cf4b7ef4bda0b90ad
Akash sharma <akash614214@gmail.com>
2026-07-03 12:51:19 +0530

    Add student search functionality by name

---
+++ Search_by_name.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+
+struct student
+{
+    int roll_no;
+    float per;
+    char name[20];
+};
+int main () {
+    struct student s[10];
+    int i, n, flag=0;
+    char search[20];
+
+    printf("Enter no.of student: ");
+    scanf("%d", &n);
+    printf("Enter detail of each student; ");
+//input
+    for(i=0;i<n;i++)
+    {
+        printf("\n---Enter detail of student %d---\n", i+1);
+        printf("Enter the roll no. of Student :  ");
+        scanf("%d", &s[i].roll_no);
+        printf("Enter the name of Student :  ");
+        getchar();
+        gets(s[i].name);
+        printf("Enter the Percentage of Student :  ");
+        scanf("%f", &s[i].per);
+    }
+
+    printf("\n\nEnter the name of student to search : ");
+    getchar();
+    gets(search);
+
+
+//output
+    for(i=0;i<n;i++)
+    {
+        if(strcmp(s[i].name, search) == 0)
+        {
+            printf("\n----Student info----\n");
+            printf("Name: %s\n", s[i].name);
+            printf("Roll no : %d\n", s[i].roll_no);
+            printf("Marks %.2f\n", s[i].per);
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("---- Student not found ! ----");
+    }
+    return 0;
+}
